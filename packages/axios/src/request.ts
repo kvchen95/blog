05feb3064faa655041c5f087814a62bf6ec2f771
gang.chen1 <gang.chen1@yintech.cn>
2023-03-09 16:23:19 +0800

    feat: axios v2

--- packages/axios/src/request.ts
+++ packages/axios/src/request.ts
@@ -10,24 +10,82 @@ type BaseApiResponse<T> = {
 // 拓展 axios 请求配置，加入我们自己的配置
 interface RequestOptions {
   // 是否全局展示请求 错误信息
-  globalErrorMessage: boolean
+  globalErrorMessage?: boolean
   // 是否全局展示请求 成功信息
-  globalSuccessMessage: boolean
+  globalSuccessMessage?: boolean
 }
 
-// 拓展请求配置
-interface ExpandInternalAxiosRequestConfig<D= any> extends InternalAxiosRequestConfig<D> {
-  requestOptions: RequestOptions
+// 拓展自定义请求配置
+interface ExpandAxiosRequestConfig<D = any> extends AxiosRequestConfig<D> {
+  interceptorHooks?: InterceptorHooks
+  requestOptions?: RequestOptions
 }
 
-// 拓展请求配置
-interface ExpandRequestConfig<D = any> extends AxiosRequestConfig<D> {
-  requestOptions: RequestOptions
+// 拓展 axios 请求配置
+interface ExpandInternalAxiosRequestConfig<D = any> extends InternalAxiosRequestConfig<D> {
+  interceptorHooks?: InterceptorHooks
+  requestOptions?: RequestOptions
 }
 
-// 拓展请求返回配置
+// 拓展 axios 返回配置
 interface ExpandAxiosResponse<T = any, D = any> extends AxiosResponse<T, D> {
   config: ExpandInternalAxiosRequestConfig<D>
+}
+
+export interface InterceptorHooks {
+  requestInterceptor?: (config: InternalAxiosRequestConfig) => InternalAxiosRequestConfig
+  requestInterceptorCatch?: (error: any) => any
+  responseInterceptor?: (response: AxiosResponse) => AxiosResponse | Promise<AxiosResponse>
+  responseInterceptorCatch?: (error: any) => any
+}
+
+const transform: InterceptorHooks = {
+  requestInterceptor(config) {
+    // 请求头部处理，如添加 token
+    const token = 'token-value'
+    if (token) {
+      config!.headers!.Authorization = token
+    }
+    return config
+  },
+  requestInterceptorCatch(err) {
+    // 请求错误，这里可以用全局提示框进行提示
+    return Promise.reject(err)
+  },
+  responseInterceptor(result) {
+    // 因为 axios 返回不支持扩展自定义配置，需要自己断言一下
+    const res = result as ExpandAxiosResponse
+    // 与后端约定的请求成功码
+    const SUCCESS_CODE = 1
+    if (res.status !== 200) return Promise.reject(res)
+    if (res.data.code !== SUCCESS_CODE) {
+      if (res.config.requestOptions?.globalErrorMessage) {
+        // 这里全局提示错误
+        console.error(res.data.message)
+      }
+      return Promise.reject(res.data)
+    }
+    if (res.config.requestOptions?.globalSuccessMessage) {
+      // 这里全局提示求求成功
+      console.log(res.data.message)
+    }
+    // 请求返回值，建议将 返回值 进行解构
+    return res.data.result
+  },
+  responseInterceptorCatch(err) {
+    // 这里用来处理 http 常见错误，进行全局提示
+    const mapErrorStatus = new Map([
+      [400, '请求方式错误'],
+      [401, '请重新登录'],
+      [403, '拒绝访问'],
+      [404, '请求地址有误'],
+      [500, '服务器出错']
+    ])
+    const message = mapErrorStatus.get(err.response.status) || '请求出错，请稍后再试'
+    // 此处全局报错
+    console.error(message)
+    return Promise.reject(err.response)
+  }
 }
 
 // 导出Request类，可以用来自定义传递配置来创建实例
@@ -35,98 +93,80 @@ export default class Request {
   // axios 实例
   private _instance: AxiosInstance
   // 默认配置
-  private _defaultConfig: ExpandRequestConfig = {
+  private _defaultConfig: ExpandAxiosRequestConfig = {
     baseURL: '/api',
     timeout: 5000,
     requestOptions: {
       globalErrorMessage: true,
-      globalSuccessMessage: false,
-    },
+      globalSuccessMessage: false
+    }
   }
-  // TODO:
-  // 拦截器配置
-  private _interceptorHooks: any
+  private _interceptorHooks?: InterceptorHooks
 
-  constructor(config: ExpandRequestConfig) {
+  constructor(config: ExpandAxiosRequestConfig) {
     // 使用axios.create创建axios实例
     this._instance = axios.create(Object.assign(this._defaultConfig, config))
+    this._interceptorHooks = config.interceptorHooks
+    this.setupInterceptors()
+  }
 
-    this._instance.interceptors.request.use(
-      (config) => {
-        // 请求头部处理，如添加 token
-        const token = 'token-value'
-        if (token) {
-          config.headers.Authorization = token
-        }
-        return config
-      },
-      <T = any>(
-        err: ExpandAxiosResponse<BaseApiResponse<T>, ExpandRequestConfig>
-      ) => {
-        console.log('err: ', err)
-        err.config.requestOptions.globalErrorMessage
-        // 请求错误，这里可以用全局提示框进行提示
-        return Promise.reject(err)
-      }
-    )
-
-    this._instance.interceptors.response.use(
-      (res: AxiosResponse) => {
-        // 请求返回值，建议将 返回值 进行解构
-        return res.data
-      },
-      (err: any) => {
-        // 这里用来处理 http 常见错误，进行全局提示
-        const mapErrorStatus = new Map([
-          [400, '请求方式错误'],
-          [401, '请重新登录'],
-          [403, '拒绝访问'],
-          [404, '请求地址有误'],
-          [500, '服务器出错'],
-        ])
-        let message =
-          mapErrorStatus.get(err.response.status) || '请求出错，请稍后再试'
-        // 此处全局报错
-        console.error(message)
-        return Promise.reject(err.response)
-      }
-    )
+  // 通用拦截，在初始化时就进行注册和运行，对基础属性进行处理
+  private setupInterceptors() {
+    this._instance.interceptors.request.use(this._interceptorHooks?.requestInterceptor, this._interceptorHooks?.requestInterceptorCatch)
+    this._instance.interceptors.response.use(this._interceptorHooks?.responseInterceptor, this._interceptorHooks?.responseInterceptorCatch)
   }
 
   // 定义核心请求
-  public request(config: ExpandRequestConfig): Promise<AxiosResponse> {
+  public request(config: ExpandAxiosRequestConfig): Promise<AxiosResponse> {
     // ！！！⚠️ 注意：axios 已经将请求使用 promise 封装过了
     // 这里直接返回，不需要我们再使用 promise 封装一层
     return this._instance.request(config)
   }
 
-  public get<T = any>(
-    url: string,
-    config?: ExpandRequestConfig
-  ): Promise<AxiosResponse<BaseApiResponse<T>>> {
+  public get<T = any>(url: string, config?: ExpandAxiosRequestConfig): Promise<AxiosResponse<BaseApiResponse<T>>> {
     return this._instance.get(url, config)
   }
 
-  public post<T = any>(
-    url: string,
-    data?: any,
-    config?: ExpandRequestConfig
-  ): Promise<AxiosResponse<BaseApiResponse<T>>> {
+  public post<T = any>(url: string, data?: any, config?: ExpandAxiosRequestConfig): Promise<T> {
     return this._instance.post(url, data, config)
   }
 
-  public put<T = any>(
-    url: string,
-    data?: any,
-    config?: ExpandRequestConfig
-  ): Promise<AxiosResponse<BaseApiResponse<T>>> {
+  public put<T = any>(url: string, data?: any, config?: ExpandAxiosRequestConfig): Promise<T> {
     return this._instance.put(url, data, config)
   }
 
-  public delete<T = any>(
-    url: string,
-    config?: ExpandRequestConfig
-  ): Promise<AxiosResponse<BaseApiResponse<T>>> {
+  public delete<T = any>(url: string, config?: ExpandAxiosRequestConfig): Promise<T> {
     return this._instance.delete(url, config)
   }
 }
+
+
+// 具体使用时先实例一个请求对象
+const request = new Request({
+  baseURL: '/api',
+  timeout: 5000,
+  interceptorHooks: transform
+})
+
+interface ResModel {
+  str: string
+  num: number
+}
+// 发起请求
+request
+  .post<ResModel>(
+    '/abc',
+    {
+      a: 'aa',
+      b: 'bb'
+    },
+    {
+      requestOptions: {
+        globalErrorMessage: true
+      }
+    }
+  )
+  .then((res) => {
+    console.log('res: ', res)
+    console.log(res.str)
+  })
